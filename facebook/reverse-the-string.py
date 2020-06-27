362355586a05603229f0621e00a6748bb1ba195a
Max Base <MaxBaseCode@Gmail.Com>
2020-06-27 18:36:45 +0430

    Create reverse-the-string.py

---
+++ facebook/reverse-the-string.py
@@ -0,0 +1,22 @@
+# Max Base
+# https://github.com/BaseMax/InterviewFAANG
+class Solution:
+	# @param A : string
+	# @return a strings
+	def solve(self, A):
+		words=A.split(" ")
+		print(words)
+		words=reversed(words)
+		result=""
+		for word in words:
+			# print(word)
+			result+=word+" "
+		result=result.strip()
+		# if result[-1] == " ":
+		# 	result=result[0:-1]
+		return result
+
+s=Solution()
+print(s.solve("the sky is blue"))
+print(s.solve("this is ib"))
+print(s.solve("qxkpvo  f   w vdg t wqxy ln mbqmtwwbaegx   mskgtlenfnipsl bddjk znhksoewu zwh bd fqecoskmo"))
