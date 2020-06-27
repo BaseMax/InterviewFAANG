105679c102f7bb0163497e73e0c20e0a8367aec3
Max Base <MaxBaseCode@Gmail.Com>
2020-06-27 19:04:13 +0430

    Update sort-by-color.py

--- facebook/sort-by-color.py
+++ facebook/sort-by-color.py
@@ -3,15 +3,32 @@
 class Solution:
     # @param A : list of integers
     # @return A after the sort
+    # Bad performance:
+    # def sortColors(self, A):
+    #     swapped = True
+    #     while swapped:
+    #         swapped = False
+    #         for i in range(len(A) - 1):
+    #             if A[i] > A[i + 1]:
+    #                 A[i], A[i + 1] = A[i + 1], A[i]
+    #                 swapped = True
+    #     return A
     def sortColors(self, A):
-        swapped = True
-        while swapped:
-            swapped = False
-            for i in range(len(A) - 1):
-                if A[i] > A[i + 1]:
-                    A[i], A[i + 1] = A[i + 1], A[i]
-                    swapped = True
-        return A
+        result1=[]
+        result2=[]
+        result3=[]
+        for i in A:
+            if i == 0:
+                result1.append(i)
+            elif i == 1:
+                result2.append(i)
+            elif i == 2:
+                result3.append(i)
+        # print(result1)
+        # print(result2)
+        # print(result3)
+        return result1+result2+result3
 
 s=Solution()
 print(s.sortColors([0, 1, 2, 0, 1, 2]))
+print(s.sortColors([ 2, 0, 0, 1, 0, 0, 2, 2, 1, 1, 0, 0, 1, 0, 2, 1, 1, 0, 1, 0, 1, 2, 2, 2, 0, 0, 1, 0, 2, 1, 1, 2, 1, 2, 2, 1, 0, 2, 2, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 2, 0, 2, 0, 1, 1, 0, 2, 2, 1, 2, 0, 2, 1, 1, 1, 2, 0, 1, 0, 2, 2, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 2, 1, 1, 0, 2, 1, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 1, 1, 0, 2, 1, 2, 2, 2, 1, 2, 2, 0, 1, 0, 1, 2, 1, 1, 0, 1, 2, 0, 1, 0, 2, 2, 1, 2, 1, 0, 2, 2, 1, 1, 0, 2, 1, 2 ]))
